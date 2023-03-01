7da91d713c460019c791a159489bb743f661639f
Bennett McCullough <mccullough.bennett@gmail.com>
2023-03-01 14:06:24 -0700

    Add project files.

---
+++ Bookstore/Models/BookstoreDatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Bookstore.Models
+{
+    public class BookstoreDatabaseContext : DbContext
+    {
+        public BookstoreDatabaseContext()
+        {
+        }
+
+        public BookstoreDatabaseContext(DbContextOptions<BookstoreDatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ Bookstore/Models/EFBookstoreRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Models
+{
+    public class EFBookstoreRepository : IBookstoreRepository
+    {
+        private BookstoreDatabaseContext context { get; set; }
+        public EFBookstoreRepository (BookstoreDatabaseContext temp)
+        {
+            context = temp;
+        }
+        public IQueryable<Book> Books => context.Books;
+    }
+}
